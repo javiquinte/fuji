835777f89dead9c858742c8c99c854b1393ddd09
Javier Quinteros <javier@quinteros.eu>
2023-05-08 15:45:58 +0200

    Improve the format of the input of the mass evaluation of DOIs
    The input file must have a DOI (as previously developed), but can also contain a name to make it easier to track for the user. This name will be used for the output filename.

--- examples/mass_assessment/fuji_mass_eval_template.py
+++ examples/mass_assessment/fuji_mass_eval_template.py
@@ -9,11 +9,16 @@ import json
 #curl -X POST "http://localhost:1071/fuji/api/v1/evaluate" -H  "accept: application/json" -H  "Authorization: Basic bWFydmVsOndvbmRlcndvbWFu" -H  "Content-Type: application/json" -d "{\"object_identifier\":\"https://archive.materialscloud.org/record/2021.146\",\"test_debug\":true,\"use_datacite\":true}"
 
 results_folder = './results/'
-pids = ['https://archive.materialscloud.org/record/2021.146']
+# pids = ['https://archive.materialscloud.org/record/2021.146']
 
 # or load pids from a file with one pid per line, which you have to generate beforehand
-#with open('dois.txt', 'r') as fileo:
-#    pids = fileo.readlines()
+# Lines starting with '#' will be skipped as comments
+# Normal lines are expected to have two fields separated by a space. For instance:
+# 10.1234/XXYYZZ MyDataset
+# The first field will be the PID and the second one the name of the dataset. This will be
+# used to store the results in a way that can be easily recognised
+with open('dois.txt', 'r') as fileo:
+    pids = fileo.readlines()
 
 fuji_api_url = 'http://localhost:1071/fuji/api/v1/evaluate'
 # the Authorization key you get from your running swagger API instance
@@ -25,13 +30,20 @@ headers = {
 base_request_dict = {'object_identifier': None, 'test_debug': True, 'use_datacite': True}
 
 # Store one file per pid for later report creation
-for pid in pids:
+for pid_name in pids:
+    # Skip lines with comments
+    if pid_name.startswith('#'):
+        continue
+    aux = pid_name.split()
+    pid = aux[0]
+    # depending on the pid you may want to change this
+    name = aux[1] if len(aux) > 1 else pid.split('/')[-1]
     req_dict = base_request_dict.copy()
     req_dict['object_identifier'] = pid
     req = requests.post(fuji_api_url, json=req_dict, headers=headers)
 
     rs_json = req.json()
-    res_filename = '{}.json'.format(pid.split('/')[-1])  # depending on the pid you may want to change this
+    res_filename = '{}.json'.format(name)
     res_filename_path = os.path.join(results_folder, res_filename)
 
     with open(res_filename_path, 'w', encoding='utf-8') as fileo:
